aba9a1cb0d59e60875b52911c34b63f50d9adb29
Buddha Nepal <44069605+ibuddhanepal@users.noreply.github.com>
2023-05-15 18:09:00 -0400

    Create get_disclosure_versions.py

---
+++ get_disclosure_versions.py
@@ -0,0 +1,20 @@
+"""Get consent disclosure versions.
+
+Retrieve the consent disclosure versions by calling /disclosure-versions API endpoint.
+"""
+
+import requests
+import output
+
+def main():
+    url = 'http://localhost:8000/disclosure-versions?disclosure_id=0'
+    headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+
+    response = requests.get(url, headers=headers)
+    if response.ok:
+        output.printTable(response.json())
+    else:
+        print(response.raise_for_status())
+
+
+if __name__ == "__main__":
